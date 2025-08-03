69af2fba2648c5b25066487a553a488bee3ff82c
Hazjel <duojhs222@gmail.com>
2025-08-03 17:32:21 +0700

    first

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="bg-blue-200 text-center p-4 mt-8">
-      <p className="text-sm">&copy; 2025 FakeStore. All rights reserved.</p>
+    <footer className="bg-black text-center p-4 mt-8">
+      <p className="text-sm text-white">&copy; 2025 FakeStore. All rights reserved.</p>
     </footer>
   );
 }
